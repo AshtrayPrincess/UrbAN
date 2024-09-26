adaab202a6a474726003f15c674f849ab076ff99
AshtrayPrincess <frunduk_lul@inbox.ru>
2024-09-26 17:22:49 +0300

    first commit

---
+++ beginer/4th.py
@@ -0,0 +1,8 @@
+# 4th program
+
+str_a = '123.456'
+b = float(str_a)
+c = ((b - int(b)) * 10)
+print(int(c % 10))
+
+
---
+++ m1/1.3.py
@@ -0,0 +1,10 @@
+#1.3 Динамическая Типизация
+name = 'Фил'
+age = 23
+new_age = 25
+is_student = True
+
+print('Name: ', name)
+print('Age: ', age)
+print('New age: ',age + (new_age - age))
+print('Is student: ', is_student)
