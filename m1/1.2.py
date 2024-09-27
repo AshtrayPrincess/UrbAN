d48516ba49326ce3c87567c1acd65d6701b51098
AshtrayPrincess <frunduk_lul@inbox.ru>
2024-09-27 16:09:46 +0300

    first commit

--- m1/1.2.py
+++ m1/1.2.py
@@ -5,4 +5,4 @@ count = "12"
 hours_spend = "1.5ч"
 avarage_time_for_complete = "0.125ч"
 
-print('на курсе',name_course,"было выполнено", count,", затраченно времени:", hours_spend,", среднее время выполнения задач: ",avarage_time_for_complete)
+print('на курсе',name_course,"было выполнено", count,"задач"", затраченно времени:", hours_spend,", среднее время выполнения задач: ",avarage_time_for_complete)
