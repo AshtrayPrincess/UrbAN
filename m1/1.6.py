be14225ee18bfdfe0545d22665165ab3157e1dde
AshtrayPrincess <frunduk_lul@inbox.ru>
2024-10-09 16:38:06 +0300

    first commit

--- m1/1.6.py
+++ m1/1.6.py
@@ -9,11 +9,15 @@ print(dct['oleg'])
 dct.update({'fenya': 1987,
         'grisha': 1977})
 del dct['sasha']
+b = dct.pop('denis')  #метод "pop" используется при том случае, когда нужно извлечь значение или ключ как в данном случае
 print(dct)
+print(b)
 
 ####
 
 st = {1,2,3,4,5,4,3,2,1, "str"}
 st.add(6)
-st.remove(1)
+st.add("trs")
+st.remove(1) #discard и remove используется для удаления элементов во множестве, различие в том что если использовать discard то программа не выдаст ошибку при ненаходе элемента, в отличии от remove
+st.discard("str")
 print(st)
